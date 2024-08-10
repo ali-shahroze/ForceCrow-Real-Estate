cbd1ba2e405fd66a6d7f9b177bf2ff91d38bebf4
Ali Shahroze <alishahroze123@gmail.com>
2024-08-11 03:37:18 +0500

    Created home and about compnents & Configured routes for them

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './CustomComponents/home/home.component';
+import { AboutComponent } from './CustomComponents/about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+        { path: '', component: HomeComponent },
+        { path: '/about', component: AboutComponent }
+      ];
