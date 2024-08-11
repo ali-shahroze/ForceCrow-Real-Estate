c268f161e21ac5f2fd2104ae6f433b09dc1ca4ab
Ali Shahroze <alishahroze123@gmail.com>
2024-08-12 03:06:03 +0500

    Footer Done

--- src/app/CustomComponents/cards/cards.component.ts
+++ src/app/CustomComponents/cards/cards.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
-
+import { CardData } from '../card-data';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-cards',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './cards.component.html',
   styleUrl: './cards.component.css'
 })
 export class CardsComponent {
-
+  cards: CardData[] = [
+    new CardData(
+      "https://forcecrow.com/wp-content/uploads/2024/08/dubai-scaled.webp",
+      "Card image cap",
+      "Dubai",
+      "Discover the futuristic skyline and unparalleled luxury of Dubai, where modernity meets opulence."
+    ),
+    new CardData(
+      "https://forcecrow.com/wp-content/uploads/2024/08/maxico-scaled.webp",
+      "Card image cap",
+      "Mexico",
+      "Experience the vibrant culture and breathtaking landscapes of Mexico, a destination where every corner tells a story."
+    ),
+    new CardData(
+      "https://forcecrow.com/wp-content/uploads/2024/08/london-1-scaled.webp",
+      "Card image cap",
+      "London",
+      "Immerse yourself in the timeless charm and rich history of London, a city where tradition and innovation coexist."      
+    )
+  ];
 }
--- src/app/CustomComponents/home/home.component.ts
+++ src/app/CustomComponents/home/home.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
 import { HeroTextBlockComponent } from "../hero-text-block/hero-text-block.component";
 import { CardsComponent } from "../cards/cards.component";
+import { FooterComponent } from "../footer/footer.component";
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavBarComponent, HeroTextBlockComponent, CardsComponent],
+  imports: [NavBarComponent, HeroTextBlockComponent, CardsComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
